ba51b6f32be0a019d60fc62374b3036e55342678
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-04-29 14:47:40 -0300

    Update and rename

---
+++ Generic exercises/SumOfNumbersUsingFor.py
@@ -0,0 +1,8 @@
+#Write a Python program to calculate the sum of all numbers from 1 to n using a for loop.
+#My code:
+
+def sum_to_n(n):
+    m = 0
+    for i in range(1, n + 1):
+        m += i
+    return m
