fa8e990464849c2a1c3a38a34bfec978cbc47718
yatomikazuchi <caioevilabner@gmail.com>
2024-04-28 23:21:11 -0300

    updated

---
+++ Generic exercises/EvenList.py
@@ -0,0 +1,16 @@
+#Crie uma função que receba uma lista de números e retorne outra lista contendo apenas os números pares da lista original.
+#My code:
+
+def EvenList(numbers):
+    pares = []
+    for n in numbers:
+        n % 2
+        if n == 0:
+            pares.append()
+        return
+listw = [10, 322, 1235, 623, 1267, 235, 6433, 1685, 9245]
+
+EvenNumbers = EvenList(listw)
+print(EvenNumbers)
+
+#WIP
