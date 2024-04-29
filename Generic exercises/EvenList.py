22c3fe1bae18887238bcc3b20df60222d731a977
yatomikazuchi <caioevilabner@gmail.com>
2024-04-29 00:11:08 -0300

    updated

--- Generic exercises/EvenList.py
+++ Generic exercises/EvenList.py
@@ -4,13 +4,13 @@
 def EvenList(numbers):
     pares = []
     for n in numbers:
-        n % 2
-        if n == 0:
-            pares.append()
-        return
+        n_operacional = n % 2
+        if n_operacional == 0:
+            pares.append(n)
+        else:
+            pass
+    return pares
 listw = [10, 322, 1235, 623, 1267, 235, 6433, 1685, 9245]
 
 EvenNumbers = EvenList(listw)
 print(EvenNumbers)
-
-#WIP
