f0cff6c1dfddf3ed84e79e6812f9559a7d385d85
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-04-29 09:30:54 -0300

    Update MaximumMinimum.py

--- Generic exercises/MaximumMinimum.py
+++ Generic exercises/MaximumMinimum.py
@@ -1,1 +1,17 @@
+#Find Maximum and Minimum: Write a Python program to find the maximum and minimum numbers in a list.
+#My code:
 
+def max_min(numbers):
+    lista = []
+    lista.extend(numbers)
+    lista.sort()
+    min_val = lista[0]
+    max_val = lista[-1]
+    listafinal = []
+    listafinal.append(min_val)
+    listafinal.append(max_val)
+    return listafinal
+
+numeroswww = [3, 2, 5, 1, 2, 6, 12, 4, 23, 521, 342, 54, 2345, 234, 2364]
+numeros = max_min(numeroswww)
+print(f"The min and max are respective: {numeros}")
