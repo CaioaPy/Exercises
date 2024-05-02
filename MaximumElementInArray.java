3dcbafb690f7a8ab49af7b27315576a55f91d1f1
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-05-02 10:20:48 -0300

    Update MaximumElementInArray.java

--- MaximumElementInArray.java
+++ MaximumElementInArray.java
@@ -1,1 +1,24 @@
+//Write a Java program to find the largest element in an array.
 
+import java.util.*;
+
+public class MaximumElementInArray {
+	
+    public static void main(String[] args) {
+        int x;
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        lista.add(2708);
+        lista.add(287);
+        lista.add(5432);
+        lista.add(5672);
+        lista.add(122);
+        lista.add(289);
+        lista.add(273);
+        lista.add(245);
+        lista.add(268);
+        lista.add(23);
+        Collections.sort(lista);
+        x = lista.get(lista.size() - 1);
+        System.out.println(x);
+    }
+}
