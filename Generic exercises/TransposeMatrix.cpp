e21a875c01453955ff81c6a0d3deebceb9ce8e2d
yatomikazuchi <caioevilabner@gmail.com>
2024-05-18 22:50:44 -0300

    updated

--- Generic exercises/TransposeMatrix.cpp
+++ Generic exercises/TransposeMatrix.cpp
@@ -4,15 +4,19 @@
 using namespace std;
 
 int main() {
-    AList [3][3] = {{3, 5, 7},
+    int AList [3][3] = {{3, 5, 7},
                     {1, 2, 3},
                     {3, 6, 9}
                     };
-    BList [3][3];
+    int BList [3][3];
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            
+            BList[j][i] = AList[i][j];
         }
     }
-    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << BList[i][j] << " ";
+        } cout << endl;
+    }
 }
