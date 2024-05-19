915afc10110083528361e8f43ea7425025d4cf08
yatomikazuchi <caioevilabner@gmail.com>
2024-05-18 22:47:44 -0300

    updated

---
+++ Generic exercises/TransposeMatrix.cpp
@@ -0,0 +1,18 @@
+//Write a function to transpose a 3x3 matrix.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    AList [3][3] = {{3, 5, 7},
+                    {1, 2, 3},
+                    {3, 6, 9}
+                    };
+    BList [3][3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            
+        }
+    }
+    
+}
