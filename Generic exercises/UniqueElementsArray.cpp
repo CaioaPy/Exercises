4baf1b5a1b65eef893ea447c19f12b8cacac40db
yatomikazuchi <caioevilabner@gmail.com>
2024-05-25 17:26:05 -0300

    updated

---
+++ Generic exercises/UniqueElementsArray.cpp
@@ -0,0 +1,25 @@
+//Create a program that takes a list of numbers and returns a new list with unique elements of the first list.
+#include <iostream>
+
+using namespace std;
+
+void UniqueElements(int arr, int N){
+    for (int i = 0; i < N; i++){
+        int j;
+        for (int j = 0; j < i; j++){
+            if (arr[i] == arr[j]){
+                break;
+            }
+        }
+        if (i == j){
+            cout << arr[i] << " ";
+        }
+    }
+
+}
+
+int main() {
+    int lista[] = {1,2,4,2,3,5,1,2,3,6,5};
+    int N = sizeof(lista) / sizeof(lista[0]);
+    UniqueElements(lista, N);
+}
