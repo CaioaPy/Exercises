c147628c7bb52e02989f39d9cb9759fffc0d98ad
yatomikazuchi <caioevilabner@gmail.com>
2024-05-26 20:05:09 -0300

    updated

--- Generic exercises/DiagonalSum.cpp
+++ Generic exercises/DiagonalSum.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main() {
-    int Matrix[3][3] = {    {1, 2, 3},
+    int Matrix[3][3] = {    {2, 2, 3},
                             {4, 5, 6},
-                            {7, 8, 9}
+                            {6, 8, 9}
                     };
     int sum;
     for (int i = 0; i < 3; i++) {
