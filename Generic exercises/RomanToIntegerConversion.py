2c0aa9dab8d92c05d594554549763a453dcd8b77
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-06-04 09:45:49 -0300

    Update RomanToIntegerConversion.py

--- Generic exercises/RomanToIntegerConversion.py
+++ Generic exercises/RomanToIntegerConversion.py
@@ -1,1 +1,36 @@
+#Roman to Integer Conversion
 
+I = 1;
+V = 5;
+X = 10;
+L = 50;
+C = 100;
+D = 500;
+M = 1000;
+
+numberInStr = input("insert the number in roman numerals: \n");
+number = 0;
+chars = list(numberInStr);
+
+for i in numberInStr:
+    if (chars[i] == "I"):
+        number += I;
+    
+    elif (chars[i] == 'V'):
+        number += V;
+    
+    elif (chars[i] == 'X'):
+        number += X;
+    
+    elif (chars[i] == 'L'):
+        number += L;
+    
+    elif (chars[i] == 'C'):
+        number += C
+    
+    elif (chars[i] == 'D'):
+        number += D
+    
+    elif (chars[i] == 'M'):
+        number += M
+    
