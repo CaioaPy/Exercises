e06a2f3de7dc7e925da580cd3e607a4c8b81b88f
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-06-06 09:28:39 -0300

    Update PasswordGenerator.py

--- Generic exercises/PasswordGenerator.py
+++ Generic exercises/PasswordGenerator.py
@@ -1,14 +1,16 @@
 #Write a Python program to generate a random password of a specified length input by the user. 
 #The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
 import random
+import string
 
-elements = ["a"]
+elements = [char for char in string.ascii_letters + string.digits + "!@#$%&*"]
 
 def criar(i):
     r = 0
     for r in range(0, i):
-        x = random.randint(0, 30)
-        password.append(x)
+        x = random.randint(0, 85)
+        y = elements[x]
+        password.append(y)
         r += 1
 
 
@@ -16,4 +18,4 @@ print("welcome to the password generator!")
 i = int(input("insert the length for the password: "))
 password = []
 criar(i)
-print(password)
+print("".join(password))
