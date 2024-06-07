a6c2cc1a71f967f8c080953e79483e965b2ee5da
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-06-07 11:16:56 -0300

    Update 1639. number of ways to form a target string given a dictionary.py

--- Leetcode/1639. number of ways to form a target string given a dictionary.py
+++ Leetcode/1639. number of ways to form a target string given a dictionary.py
@@ -1,1 +1,21 @@
-
+def numWays(self, words: List[str], target: str) -> int:
+    ways = 0
+    for c in target:
+        targetC = list(target)
+        i = 0
+        j = 0
+        word = []
+        while i < 5 and j < 5:
+            if (c == words[i][j]):
+                word.append(words[i][j])
+            elif (word == targetC):
+                ways += 1
+                word.pop(0)
+                word.pop(0)
+                word.pop(0)
+            
+    return ways
+                
+words = ["acca","bbbb","caca", "eeww", "fksa"]
+target = "fae"
+numWays(words, target)
