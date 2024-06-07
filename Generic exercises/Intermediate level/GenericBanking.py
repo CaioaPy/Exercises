4fe0f012bf494a05f34aa8f18e9c3e6dfeea7793
yatomikazuchi <caioevilabner@gmail.com>
2024-06-07 19:29:05 -0300

    updated

--- Generic exercises/Intermediate level/GenericBanking.py
+++ Generic exercises/Intermediate level/GenericBanking.py
@@ -23,13 +23,20 @@ class BankAccount():
     def __str__(self):
         return f"{self.__class__.__name__}: {", ".join([f'{key}: {value}'for key, value in self.__dict__.items()])}"
     
-    def deposit(self, deposit):
-        print(f"depositing: ${deposit} into account {self.account_number}")
-        self.balance += deposit
-        print(f"new balance: ${self.balance}")
+    def deposit(self, deposit_amount):
+        print(f"Depositing: ${deposit_amount} into account: {self.account_number}")
+        self.balance += deposit_amount
+        print(f"New balance: ${self.balance}")
+    
+    def withdraw(self, withdraw_amount):
+        print(f"Withdrawing: ${withdraw_amount} from account: {self.account_number}")
+        self.balance -= withdraw_amount
+        print(f"New balance: ${self.balance}")
+
 
     
 account1 = BankAccount(1, "yato", 156.52)
 account2 = BankAccount(2, "master", 253.71)
 
 account1.deposit(500)
+account2.withdraw(100)
