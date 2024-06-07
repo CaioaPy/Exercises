49a8f98dee67d0b5309224601cacbaaf773ac75c
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-06-07 10:51:00 -0300

    Update GenericBanking.py

--- Generic exercises/Intermediate level/GenericBanking.py
+++ Generic exercises/Intermediate level/GenericBanking.py
@@ -20,8 +20,8 @@ class BankAccount():
         self.account_holder = account_holder
         self.balance = balance
 
-    def __str__(self):
-        return f"{self.__class__.__name__}: {", ".join([f'{key}: {value}'for key, value in self.__dict__.items()])}"
+#    def __str__(self):
+        #return f"{self.__class__.__name__}: {", ".join([f'{key}: {value}'for key, value in self.__dict__.items()])}"
     
     def deposit(self, deposit):
         print(f"Depositing: ${deposit} into account: {self.account_number}")
@@ -40,8 +40,10 @@ class BankAccount():
         print(f"New balance: ${self.balance}")
     
 account1 = BankAccount(1, "yato", 156.52)
-account2 = BankAccount(2, "master", 253.71)
+account2 = BankAccount(2, "master", 253.70)
 
 account1.deposit(500)
 account2.withdraw(100)
 account1.transfer(200, account2)
+print(account1.balance)
+print(account2.balance)
