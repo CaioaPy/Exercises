6b32606da8dd2e7dedc3db859b084dc848de63af
yatomikazuchi <caioevilabner@gmail.com>
2024-06-08 23:19:50 -0300

    updated

---
+++ Generic exercises/Intermediate level/ManipulatingJSON.py
@@ -0,0 +1,8 @@
+#Objective: Parse and manipulate JSON data.
+
+#Instructions:
+
+#Load a JSON string representing a collection of users with attributes like name, age, and email.
+#Convert the JSON string into a Python dictionary.
+#Extract and print the names and emails of users who are over 25 years old.
+#Add a new user to the dictionary and convert it back to a JSON string.
