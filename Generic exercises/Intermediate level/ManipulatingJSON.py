6d194ef5cec303c1bd2f6d52452e22ee6cf1ccc7
yatomikazuchi <caioevilabner@gmail.com>
2024-06-08 23:22:21 -0300

    updated

--- Generic exercises/Intermediate level/ManipulatingJSON.py
+++ Generic exercises/Intermediate level/ManipulatingJSON.py
@@ -6,3 +6,7 @@
 #Convert the JSON string into a Python dictionary.
 #Extract and print the names and emails of users who are over 25 years old.
 #Add a new user to the dictionary and convert it back to a JSON string.
+
+import json
+
+users = '{ "name":"Master", "age":"24", "city":"NY"}'
