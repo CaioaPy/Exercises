21dfe3850eb1e70604a1856ad61cc08791172890
yatomikazuchi <caioevilabner@gmail.com>
2024-06-09 18:35:48 -0300

    new file

---
+++ Generic exercises/Intermediate level/ManipulatingCSV.py
@@ -0,0 +1,8 @@
+#Objective: Read from and write to CSV files using Python.
+
+#Instructions:
+
+#Read data from a CSV file containing columns name, age, and salary.
+#Compute the average salary of all individuals in the file.
+#Write a new CSV file that includes only individuals with a salary above the average.
+
