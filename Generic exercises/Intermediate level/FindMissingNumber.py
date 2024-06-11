43250b97b9c05254fe72142b078eb89493585f01
yatomikazuchi <caioevilabner@gmail.com>
2024-06-11 16:03:08 -0300

    updated

--- Generic exercises/Intermediate level/FindMissingNumber.py
+++ Generic exercises/Intermediate level/FindMissingNumber.py
@@ -4,7 +4,14 @@
 
 #Create a function find_missing_number(arr: List[int]) -> int that finds the missing number in an array of consecutive integers.
 
-def find_missing_number(List[int]):
+def find_missing_number(List) -> int:
+    r = len(List)
+    i = 0
+    for n in List:
+        if (n != i):
+            return i
+        else:
+            i += 1
 
-eg = [1,2,3,4,6,7,8,9]
+eg = [0,1,2,3,4,6,7,8,9]
 print(find_missing_number(eg))
