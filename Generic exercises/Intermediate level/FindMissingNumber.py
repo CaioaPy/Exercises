46afc1bd4d3db94e5557e881eefeafe02aee5187
yatomikazuchi <caioevilabner@gmail.com>
2024-06-11 15:56:17 -0300

    updated

---
+++ Generic exercises/Intermediate level/FindMissingNumber.py
@@ -0,0 +1,6 @@
+#Objective: Find the missing number in an array of consecutive integers.
+
+#Instructions:
+
+#Create a function find_missing_number(arr: List[int]) -> int that finds the missing number in an array of consecutive integers.
+
