40453f35da0f140dba696d6334f3df65255fafa1
yatomikazuchi <caioevilabner@gmail.com>
2024-06-16 16:35:20 -0300

    updated

--- Leetcode/345. Reverse Vowels of a String.py
+++ Leetcode/345. Reverse Vowels of a String.py
@@ -3,19 +3,16 @@
 #The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 
 s = "hello"
-ss = list(s)
 vowels = ["a", "e", "i", "o", "u"]
 
 def reverseVowels(str) -> str:
+    ss = list(str)
     aux = "x"
     i = 0
     aux_i = 0
-    for c in str:
-        if (aux in vowels):
-            ss[aux_i] = aux
-            print("a")
-
-        if (c == vowels):
+    for c in ss:
+        if (c == vowels[0] or c == vowels[1] or c == vowels[2] or c == vowels[3] or c == vowels[4]):
+            c = ss[aux_i]
             aux = c
             aux_i = i
         i += 1
