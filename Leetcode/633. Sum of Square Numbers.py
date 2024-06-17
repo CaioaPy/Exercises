fb439b3cb9e1cd0b01be9d8e8adb81065fba2908
yatomikazuchi <caioevilabner@gmail.com>
2024-06-17 17:45:55 -0300

    updated

--- Leetcode/633. Sum of Square Numbers.py
+++ Leetcode/633. Sum of Square Numbers.py
@@ -5,14 +5,16 @@
 def checksum(num) -> bool:
     a = 10
     b = 1
-    for n in range(1, num):
+    for n in range(1, num + num):
         c = a*a + b*b
         print(c)
         if (c > num):
-            a - 1
+            a -= 1
             print(c)
         elif (c < num):
-            b + 1
+            b += 1
             print(c)
+        elif (c == num):
+            return True
 
 print(checksum(5))
