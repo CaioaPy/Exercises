cfc28ab7b139f8ec76633e725acbc2e3684b6e7a
yatomikazuchi <caioevilabner@gmail.com>
2024-06-24 22:47:25 -0300

    updated

--- Leetcode/1038. Binary Search Tree to Greater Sum Tree.py
+++ Leetcode/1038. Binary Search Tree to Greater Sum Tree.py
@@ -6,3 +6,10 @@
 #The left subtree of a node contains only nodes with keys less than the node's key.
 #The right subtree of a node contains only nodes with keys greater than the node's key.
 #Both the left and right subtrees must also be binary search trees.
+
+#e. g.
+#Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+#Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+
+
+def BstToGst(BstRoot):
