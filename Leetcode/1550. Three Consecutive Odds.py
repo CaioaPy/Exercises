6d8245102ec92eddc82a0dbdb4e0d038afc7fcf8
yatomikazuchi <caioevilabner@gmail.com>
2024-06-30 22:53:01 -0300

    updated

--- Leetcode/1550. Three Consecutive Odds.py
+++ Leetcode/1550. Three Consecutive Odds.py
@@ -9,9 +9,16 @@ arr = [5, 1, 6, 2, 5, 7, 9, 4, 3, 1]
 
 def threeConsecutiveOdds(nums) -> bool:
     found = False
-    n1 = 0
-    n2 = 0
-    n3 = 0
+    n1 = None
+    n2 = None
+    n3 = None
     for n in nums:
         if n % 2 == 1:
             n1 = n
+        elif n1 != 0 and n2 != 0 and n3 != 0:
+            return True
+        else:
+            n1 = 0
+            n2 = 0
+            n3 = 0
+    return False
