8b82bffaac3a8d3d30b6ebf0425531ed3c5ba94a
yatomikazuchi <caioevilabner@gmail.com>
2024-07-02 13:57:10 -0300

    updated

--- Leetcode/1774. Closest Dessert Cost.py
+++ Leetcode/1774. Closest Dessert Cost.py
@@ -31,20 +31,23 @@ def ClosestCost(bases, tops, target) -> int:
     for n in bases:
         x = 0
         x += n
-        loop = True
-        while loop:
+        loop = 0
+        leng = len(bases) - 1
+        while loop < leng:
             x2 = x
             i = 0
             len_bases = len(tops)
             Iloop = 0
             while Iloop < len_bases:
-                if x2 + tops[i] < target:
+                if x2 + tops[i] <= target:
                     x2 += tops[i]
-                elif x2 + tops[i] > target:
+                elif x2 + tops[i] >= target:
                     final_costs.append(x2)
                     i += 1
                     Iloop += 1
+            loop += 1
     final_costs.sort()
+    print(final_costs)
     for n in final_costs:
         if n == target:
             return n
