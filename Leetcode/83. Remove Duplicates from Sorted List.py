38187f1e633820c01bd8db063cd5adaf31c9acd0
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-08-21 09:42:53 -0300

    updated

--- Leetcode/83. Remove Duplicates from Sorted List.py
+++ Leetcode/83. Remove Duplicates from Sorted List.py
@@ -1,1 +1,13 @@
+def RemoveDuplicates(nums):
+    rang = range(0, len(nums) - 1)
+    for n in rang:
+        val = nums[n - 1]
+        next = nums[n]
+        if val == next:
+            nums.remove(next)
 
+
+numeros = [1,2,3,4,4,5,6,7]
+print(numeros)
+RemoveDuplicates(numeros)
+print(numeros)
