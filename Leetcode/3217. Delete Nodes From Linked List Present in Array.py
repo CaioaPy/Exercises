1d94ca216ff036a0613fde1b6518527f480f553b
yatomikazuchi <caioevilabner@gmail.com>
2024-09-06 22:28:16 -0300

    new file

---
+++ Leetcode/3217. Delete Nodes From Linked List Present in Array.py
@@ -0,0 +1,9 @@
+#You are given an array of integers nums and the head of a linked list. 
+#Return the head of the modified linked list after removing all nodes from the linked list that have a value that exists in nums.
+
+#Example 1:
+
+#Input: nums = [1,2,3], head = [1,2,3,4,5]
+
+#Output: [4,5]
+
