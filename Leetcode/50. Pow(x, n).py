c42d9811900fc2bd9d36443010f114d227561059
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-10-24 12:49:42 -0300

    Update 50. Pow(x, n).py

--- Leetcode/50. Pow(x, n).py
+++ Leetcode/50. Pow(x, n).py
@@ -1,1 +1,22 @@
+#Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
+#Example 1:
+#Input: x = 2.00000, n = 10
+#Output: 1024.00000
+
+#Example 2:
+#Input: x = 2.10000, n = 3
+#Output: 9.26100
+
+
+def myPow(x: float, n: int) -> float:
+    final = x
+    n -= 1
+    while n:
+        final *= x
+        n -= 1
+    return final
+
+#test
+x = myPow(2, 3)
+print(x)
