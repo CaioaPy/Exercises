4bcc54d759138ae3bc70b9097a4b37a542ec5f17
yatomikazuchi <caioevilabner@gmail.com>
2024-12-11 22:10:20 -0300

    code

--- Leetcode/LeetCode 75/String Array/334. Increasing Triplet Subsequence.py
+++ Leetcode/LeetCode 75/String Array/334. Increasing Triplet Subsequence.py
@@ -16,3 +16,19 @@
 #Output: true
 #Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
 
+def increasingTriplet(nums: list[int]) -> bool:
+    min_val = 1000000000000000000000000000
+    second_min_val = 100000000000000000000000000000000
+    for num in nums:
+        if num <= min_val:
+            min_val = num
+        elif num <= second_min_val:
+            second_min_val = num
+        else:
+            return True
+
+    return False
+
+a = [2,1,5,0,4,6] 
+x = increasingTriplet(a)
+print(x)
