f6ccdde031e2d29a5f0476a8712555689aa0dcdb
yatomikazuchi <caioevilabner@gmail.com>
2024-12-14 08:35:20 -0300

    code

--- Leetcode/LeetCode 75/Two Pointers/392. Is Subsequence.py
+++ Leetcode/LeetCode 75/Two Pointers/392. Is Subsequence.py
@@ -10,3 +10,16 @@
 #Input: s = "axc", t = "ahbgdc"
 #Output: false
 
+def isSubsequence(s: str, t: str) -> bool:
+    s_ind = 0
+    t_ind = 0
+    while s_ind < len(s) and t_ind < len(t):
+        if s[s_ind] == t[t_ind]:
+            s_ind += 1
+        t_ind += 1
+    return s_ind == len(s)
+
+a = "aaawe"
+b = "rawasacwe"
+x = isSubsequence(a, b)
+print(x)
