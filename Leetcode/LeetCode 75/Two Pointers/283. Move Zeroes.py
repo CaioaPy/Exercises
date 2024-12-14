71189a0fec763b74cd8f9364c2f2fd76de30bce6
yatomikazuchi <caioevilabner@gmail.com>
2024-12-13 23:14:03 -0300

    code

--- Leetcode/LeetCode 75/Two Pointers/283. Move Zeroes.py
+++ Leetcode/LeetCode 75/Two Pointers/283. Move Zeroes.py
@@ -9,3 +9,14 @@
 #Output: [0]
 
 def moveZeroes(nums: list[int]) -> None:
+    non_zero_index = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            if i != non_zero_index:
+                nums[non_zero_index], nums[i] = nums[i], nums[non_zero_index]
+            non_zero_index += 1
+
+a = [1,2,0,8,5,6,0,8,3,0]
+print(a)
+x = moveZeroes(a)
+print(a)
